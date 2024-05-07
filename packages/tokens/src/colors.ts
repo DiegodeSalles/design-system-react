19c066aef96bb412081e526e4c9e6a9a5a68e4b3
Diego de Salles <diegodesallesb@gmail.com>
2024-05-06 23:31:20 -0300

    Primeiro commit

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  white: "#fff",
+  black: "#000",
+
+  gray100: "#e1e1e6",
+  gray200: "#a9a9b2",
+  gray400: "#7c7c8a",
+  gray500: "#505059",
+  gray600: "#323238",
+  gray700: "#29292e",
+  gray800: "#202024",
+  gray900: "#121214",
+
+  violet300: "#c4b5fd",
+  violet500: "#8b5cf6",
+  violet700: "#6d28d9",
+  violet900: "#4c1d95",
+};
